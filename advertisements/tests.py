59189f273e501d890e39f7d7cc910d0a8f404523
RopoloTUF <ropolotuf@kuznetsovaleksandr2013@gmail.com>
2026-08-26 13:58:54 +0400

    Создание тестов

--- advertisements/tests.py
+++ advertisements/tests.py
@@ -8,7 +8,7 @@ class MyTest(TestCase):
         self.assertTrue(True)
 
     def test_sample_view(self):
-        url = "/api/v1/test/"
+        url = "/api/advertisements/"
         client = APIClient()
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
